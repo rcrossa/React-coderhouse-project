57906015da58aa922aa74f522d154b51579d9758
Roberto Rossa <rcrossa@hotmail.com>
2022-10-22 17:35:59 -0300

    Cleanup

--- src/App.js
+++ src/App.js
@@ -5,29 +5,23 @@ import ItemListContainer from './components/containers/item_ListContainer';
 import ItemDetailsContainer from './components/itemDetail/ItemDetailsContainer';
 import Checkout from './components/checkout/Checkout';
 import Cart from './components/cart/Cart';
-import { FavoritosProvider } from './contexts/FavouritesContex';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { CacheProvider } from './contexts/CacheContext';
 import { CartProvider } from './contexts/CartContext';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter basename='/react-coderhouse-project/'>
-        <CacheProvider>
-          <CartProvider>
-            <NavbarMenu />
-            <FavoritosProvider>
-              <Routes>
-                <Route path='/' element={<ItemListContainer />} />
-                <Route path='/category/:categoryId' element={<ItemListContainer />} />
-                <Route path='/item/:id' element={<ItemDetailsContainer />} />
-                <Route path='/cart' element={<Cart />} />
-                <Route path='/checkout' element={<Checkout />} />
-              </Routes>
-            </FavoritosProvider>
-          </CartProvider>
-        </CacheProvider>
+        <CartProvider>
+          <NavbarMenu />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} />
+            <Route path='/category/:categoryId' element={<ItemListContainer />} />
+            <Route path='/item/:id' element={<ItemDetailsContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
