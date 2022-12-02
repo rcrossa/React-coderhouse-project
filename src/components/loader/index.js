d0508750ce3b0e6c86ce56ae1e57e3fda1eabf64
Roberto Rossa <rcrossa@hotmail.com>
2022-12-01 21:12:40 -0300

    Updates

--- src/components/loader/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const Loader = ({ loading }) => {
-    return (loading) ? (<Spinner animation='border' variant='primary' />) : '';
-}
-
-export default Loader;
